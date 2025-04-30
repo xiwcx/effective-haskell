bf8742a0f64528331c950d674a6d3aa0ff722596
Welch Canavan <wcanavan@Welchs-Work-MBP.local>
2025-04-30 09:46:29 -0700

    ch02

--- ch02/Zip.hs
+++ ch02/Zip.hs
@@ -1,15 +1,39 @@
+{-# LANGUAGE ParallelListComp #-}
+
 module Zip where
 
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' fnc aLst bLst =
+import Debug.Trace
+
+zipWithRecurse' :: (a -> b -> c) -> [a] -> [b] -> [c]
+-- idiomatically we should never have errors, this
+-- function is considered partial as it doesn't handle
+-- empty lists. head is generally avoided unless a filled
+-- list can be garuanteed
+zipWithRecurse' fnc aLst bLst =
   let a = head aLst
       b = head bLst
       aLst' = tail aLst
       bLst' = tail bLst
    in if null aLst || null bLst
         then []
-        else fnc a b : zipWith' fnc aLst' bLst'
+        else fnc a b : zipWithRecurse' fnc aLst' bLst'
+
+zipWithRecurse'' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithRecurse'' fnc _ [] = []
+zipWithRecurse'' fnc [] _ = []
+zipWithRecurse'' fnc (a : aLst') (b : bLst') = fnc a b : zipWithRecurse'' fnc aLst' bLst'
 
 zipWithLC :: (a -> b -> c) -> [a] -> [b] -> [c]
--- doesn't feel right using zip to replace zip? am i doing something wrong here?
-zipWithLC fnc aLst bLst = [fnc a b | (a, b) <- zip aLst bLst]
+-- doesn't feel right using zip to replace zip? is there a better option here?
+zipWithLC fnc aLst bLst = [fnc a b | a <- aLst | b <- bLst]
+
+concatMapFoldl :: (c -> d) -> [[c]] -> [d]
+-- cleaner recursion than foldr
+concatMapFoldl fnc = foldl (\cur next -> cur ++ map fnc next) []
+
+concatMapFoldr :: (Show c, Show d) => (c -> d) -> [[c]] -> [d]
+-- example of debugging, logging
+concatMapFoldr fnc = foldr (\next cur -> traceShow (next, cur) (map fnc next ++ cur)) []
+
+concatFold :: [[a]] -> [a]
+concatFold = foldr (++) []
